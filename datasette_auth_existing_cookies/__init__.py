25120f13d2b5447a4db4f0955c06309a3825203f
Simon Willison <swillison@gmail.com>
2021-03-26 11:21:59 -0700

    This should work now

--- datasette_auth_existing_cookies/__init__.py
+++ datasette_auth_existing_cookies/__init__.py
@@ -2,11 +2,13 @@ from datasette import hookimpl
 import httpx
 
 
+@hookimpl
 def actor_from_request(datasette, request):
     async def inner():
         config = datasette.plugin_config("datasette-auth-existing-cookies") or {}
         api_url = config["api_url"]
-        response = await httpx.AsyncClient().get(api_url, cookies=request.cookies)
+        async with httpx.AsyncClient() as client:
+            response = await client.get(api_url, cookies=request.cookies)
         actor = response.json()
         if actor:
             return actor
